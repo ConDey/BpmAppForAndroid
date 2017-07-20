e1f7f2a32fcf5bd94eda57b09a67d108dc2f030a
ConDey <condeywadl@126.com>
2017-07-20 22:16:50 +0800

    解决文件下载的问题

--- lib.common/src/main/java/com/eazytec/bpm/lib/common/webservice/DownloadHelper.java
+++ lib.common/src/main/java/com/eazytec/bpm/lib/common/webservice/DownloadHelper.java
@@ -30,16 +30,17 @@ import rx.schedulers.Schedulers;
 
 /**
  * 通用的下载功能
+ *
  * @author Administrator
  * @version Id: DownloadHelper, v 0.1 2017/7/11 19:49 Administrator Exp $$
  */
-public class DownloadHelper{
+public class DownloadHelper {
 
-   private static CompletionHandler mHandler;
+    private static CompletionHandler mHandler;
 
-   public static void download(final CommonActivity activity, final String id, final String name, final boolean isAutoOpen, final CompletionHandler handler ) {
+    public static void download(final CommonActivity activity, final String id, final String name, final boolean isAutoOpen, final CompletionHandler handler) {
 
-       mHandler = handler;
+        mHandler = handler;
 
         final DownloadProgressDialog dialog = new DownloadProgressDialog(activity);
         dialog.setTitle("下载");
@@ -51,7 +52,11 @@ public class DownloadHelper{
         ProgressHelper.setProgressHandler(new DownloadProgressHandler() {
             @Override
             protected void onProgress(long bytesRead, long contentLength, boolean done) {
-                dialog.setProgress((int) (bytesRead *100/ contentLength));
+
+                if (contentLength != 0) {
+                    dialog.setProgress((int) (bytesRead * 100 / contentLength));
+                }
+
                 if (done) {
                     dialog.dismiss();
                 }
@@ -59,88 +64,88 @@ public class DownloadHelper{
         });
 
 
-       new Thread(new Runnable() {
-           @Override public void run() {
-               BPMRetrofit.downloadRetrofit().create(WebApi.class).download(id)
-                       .subscribeOn(Schedulers.io())
-                       .observeOn(Schedulers.io())
-                       .subscribe(new Observer<ResponseBody>() {
-                           @Override public void onNext(ResponseBody response) {
-                               try {
-                                   InputStream is = response.byteStream();
-                                   File file = new File(Environment.getExternalStorageDirectory(), name);
-                                   FileOutputStream fos = new FileOutputStream(file);
-                                   BufferedInputStream bis = new BufferedInputStream(is);
-                                   byte[] buffer = new byte[1024];
-                                   int len;
-                                   while ((len = bis.read(buffer)) != -1) {
-                                       fos.write(buffer, 0, len);
-                                       fos.flush();
-                                   }
-                                   fos.close();
-                                   bis.close();
-                                   is.close();
+        new Thread(new Runnable() {
+            @Override public void run() {
+                BPMRetrofit.downloadRetrofit().create(WebApi.class).download(id)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.io())
+                        .subscribe(new Observer<ResponseBody>() {
+                            @Override public void onNext(ResponseBody response) {
+                                try {
+                                    InputStream is = response.byteStream();
+                                    File file = new File(Environment.getExternalStorageDirectory(), name);
+                                    FileOutputStream fos = new FileOutputStream(file);
+                                    BufferedInputStream bis = new BufferedInputStream(is);
+                                    byte[] buffer = new byte[1024];
+                                    int len;
+                                    while ((len = bis.read(buffer)) != -1) {
+                                        fos.write(buffer, 0, len);
+                                        fos.flush();
+                                    }
+                                    fos.close();
+                                    bis.close();
+                                    is.close();
 
-                                   if (mHandler != null) {
-                                       fileHandler(true, null);
-                                   }
+                                    if (mHandler != null) {
+                                        fileHandler(true, null);
+                                    }
 
-                                   if (isAutoOpen) {
-                                       if (Build.VERSION.SDK_INT >= 24) {
-                                           // Android 7.0 需要用FileProvider的方式来将uri给外部应用使用
-                                           Uri uri = FileProvider.getUriForFile(activity.getContext(), "android.support.v4.content.FileProvider", file);
-                                           Intent intent = new Intent("android.intent.action.VIEW");
-                                           intent.addCategory("android.intent.category.DEFAULT");
-                                           intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                                           intent.setDataAndType(uri, MIMETypeUtil.getMIMEType(file));
-                                           activity.startActivity(intent);
-                                       } else {
-                                           Intent intent = new Intent("android.intent.action.VIEW");
-                                           intent.addCategory("android.intent.category.DEFAULT");
-                                           intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                           Uri uri = Uri.fromFile(file);
-                                           intent.setDataAndType(uri, MIMETypeUtil.getMIMEType(file));
-                                           activity.startActivity(intent);
-                                       }
-                                   }
-                               } catch (IOException e) {
-                                   e.printStackTrace();
-                               }
-                           }
+                                    if (isAutoOpen) {
+                                        if (Build.VERSION.SDK_INT >= 24) {
+                                            // Android 7.0 需要用FileProvider的方式来将uri给外部应用使用
+                                            Uri uri = FileProvider.getUriForFile(activity.getContext(), "android.support.v4.content.FileProvider", file);
+                                            Intent intent = new Intent("android.intent.action.VIEW");
+                                            intent.addCategory("android.intent.category.DEFAULT");
+                                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                            intent.setDataAndType(uri, MIMETypeUtil.getMIMEType(file));
+                                            activity.startActivity(intent);
+                                        } else {
+                                            Intent intent = new Intent("android.intent.action.VIEW");
+                                            intent.addCategory("android.intent.category.DEFAULT");
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            Uri uri = Uri.fromFile(file);
+                                            intent.setDataAndType(uri, MIMETypeUtil.getMIMEType(file));
+                                            activity.startActivity(intent);
+                                        }
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
 
-                           @Override public void onCompleted() {
-                           }
+                            @Override public void onCompleted() {
+                            }
 
-                           @Override public void onError(Throwable e) {
-                               if (mHandler != null) {
-                                   fileHandler(false, null);
-                               }
-                               ToastDelegate.error(activity.getContext(),"文件下载失败，请稍后再试");
-                               dialog.dismiss();
-                           }
-                       });
-           }
-       }).start();
-   }
+                            @Override public void onError(Throwable e) {
+                                if (mHandler != null) {
+                                    fileHandler(false, null);
+                                }
+                                ToastDelegate.error(activity.getContext(), "文件下载失败，请稍后再试");
+                                dialog.dismiss();
+                            }
+                        });
+            }
+        }).start();
+    }
 
     public static void fileHandler(boolean isSuccess, JSONObject jsonObject) {
         if (jsonObject == null) {
             jsonObject = new JSONObject();
         }
         if (isSuccess) {
-                try {
-                    jsonObject.put("success", true);
-                    jsonObject.put("errorMsg", "");
-                    mHandler.complete(jsonObject.toString());
-                }catch (JSONException e) {
-                    e.printStackTrace();
-                }
-        }else {
+            try {
+                jsonObject.put("success", true);
+                jsonObject.put("errorMsg", "");
+                mHandler.complete(jsonObject.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
             try {
                 jsonObject.put("success", false);
                 jsonObject.put("errorMsg", "");
                 mHandler.complete(jsonObject.toString());
-            }catch (JSONException e) {
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
