2765a60d10bc48c32eb5718559307d71776dafb0
Zjiahe <Zjiahe@yahoo.com>
2017-09-28 11:02:45 +0800

    日程

--- app.calendar/src/main/java/com/eazytec/bpm/app/calendar/MainActivity.java
+++ app.calendar/src/main/java/com/eazytec/bpm/app/calendar/MainActivity.java
@@ -1,45 +1,83 @@
 package com.eazytec.bpm.app.calendar;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.eazytec.bpm.app.calendar.adapters.ItemListAdapter;
+import com.eazytec.bpm.app.calendar.contact.ItemListContract;
+import com.eazytec.bpm.app.calendar.contact.ItemListPresenter;
+import com.eazytec.bpm.app.calendar.dataobject.EventListDataObject;
 import com.eazytec.bpm.appstub.view.calendar.CaledarAdapter;
 import com.eazytec.bpm.appstub.view.calendar.CalendarBean;
 import com.eazytec.bpm.appstub.view.calendar.CalendarDateView;
 import com.eazytec.bpm.appstub.view.calendar.CalendarView;
+import com.eazytec.bpm.lib.common.activity.ContractViewActivity;
 
-import static com.eazytec.bpm.lib.utils.SizeUtils.dp2px;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ContractViewActivity<ItemListPresenter> implements ItemListContract.View {
 
+    private Toolbar toolbar;
+    private TextView toolbarTitleTextView;
     private CalendarDateView calendarDateView;
 
     private TextView dateTv;
 
-    private ListView listView;
+    private ListView todayListView;
+
+    private TextView emptyTv;
+
+    private List<EventListDataObject>  eventListDataObjectList;
+
+    private ItemListAdapter itemListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_list);
-
+        String name = getIntent().getStringExtra("name");
         calendarDateView = (CalendarDateView) findViewById(R.id.schedule_list_calendarDateView);
         dateTv = (TextView) findViewById(R.id.schedule_list_date);
-        listView  = (ListView) findViewById(R.id.schedule_list);
-
+        todayListView  = (ListView) findViewById(R.id.schedule_list);
+        emptyTv=(TextView)findViewById(R.id.schedule_list_empty);
+        toolbar = (Toolbar) findViewById(R.id.bpm_toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_common_left_back);
+        toolbarTitleTextView = (TextView) findViewById(R.id.bpm_toolbar_title);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbarTitleTextView.setText(name);
         initDate();
         setListener();
+    }
 
+    @Override
+    protected ItemListPresenter createPresenter() {
+        return new ItemListPresenter();
     }
 
     private void initDate(){
+
+        itemListAdapter=new ItemListAdapter(getContext());
+        todayListView.setAdapter(itemListAdapter);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
+
         calendarDateView.setAdapter(new CaledarAdapter() {
             @Override
             public View getView(View convertView, ViewGroup parentView, CalendarBean bean) {
@@ -61,8 +99,12 @@ public class MainActivity extends Activity {
                 return convertView;
             }
         });
+        String todaydate=dateTv.getText().toString();
+        getPresenter().loadItemById(todaydate);
 
-        //这样重写的啊，只是示例
+
+
+       /* //这样重写的啊，只是示例
         listView.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() {
@@ -86,11 +128,11 @@ public class MainActivity extends Activity {
                 }
 
                 TextView textView = (TextView) convertView;
-                textView.setText("item" + position);
+                textView.setText("position:" + position);
 
                 return convertView;
             }
-        });
+        });*/
     }
 
 
@@ -99,10 +141,24 @@ public class MainActivity extends Activity {
             @Override
             public void onItemClick(View view, int postion, CalendarBean bean) {
                 dateTv.setText(bean.year + "/" + getDisPlayNumber(bean.moth) + "/" + getDisPlayNumber(bean.day));
+                String date = bean.year + "/" + getDisPlayNumber(bean.moth) + "/" + getDisPlayNumber(bean.day);
+                getPresenter().loadItemById(date);
+            }
+        });
+
+        todayListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent();
+                /*intent.putExtra();
+                intent.setClassName(getContext(),);*/
+                startActivity(intent);
+
             }
         });
     }
 
+    
     private String getDisPlayNumber(int num) {
         return num < 10 ? "0" + num : "" + num;
     }
@@ -112,4 +168,17 @@ public class MainActivity extends Activity {
         final float scale =r.getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
+
+    @Override
+    public void loadItemList(EventListDataObject eventListDataObject) {
+        if (eventListDataObject.getDatas()!=null&& eventListDataObject.getDatas().size()>0){
+            emptyTv.setVisibility(View.GONE);
+            eventListDataObjectList=eventListDataObject.getDatas();
+            itemListAdapter.resetList(eventListDataObjectList);
+            itemListAdapter.notifyDataSetChanged();
+        }else {
+            emptyTv.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
---
+++ app.calendar/src/main/java/com/eazytec/bpm/app/calendar/dataobject/EventListDataObject.java
@@ -0,0 +1,112 @@
+package com.eazytec.bpm.app.calendar.dataobject;
+
+import com.eazytec.bpm.lib.common.webservice.WebDataTObject;
+
+import java.util.List;
+
+/**
+ * Created by Vivi on 2017/9/27.
+ */
+
+public class EventListDataObject extends WebDataTObject{
+
+    private String startTime;
+
+    private String id;
+
+    private String startDate;
+
+    private String location;
+
+    private String description;
+
+    private String endDate;
+
+    private String eventType;
+
+    private String eventName;
+
+    private String endTime;
+
+    private List<EventListDataObject> datas;
+
+    public List<EventListDataObject> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<EventListDataObject> datas) {
+        this.datas = datas;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+}
